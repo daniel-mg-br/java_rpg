472b50ac0a415ffe4a1535d9483e23bbde7415f7
Daniel M. Garcia <mendoncad38@gmail.com>
2026-02-17 19:32:41 -0300

    Adição nas interações com itens
    Métodos de remover itens equipados e itens do inventário, de usar itens consumíveis e para visualizar o inventário

--- src/com/javarpg/model/entities/Character.java
+++ src/com/javarpg/model/entities/Character.java
@@ -3,9 +3,12 @@ package com.javarpg.model.entities;
 // Importando os outros diretórios do projeto
 import com.javarpg.model.items.Item;
 import com.javarpg.model.items.Equipment;
+import com.javarpg.model.items.Consumable;
 // Importando as estruturas de dados necessárias
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 // Classe abstrata que serve de molde para as outras classes
@@ -18,8 +21,9 @@ public abstract class Character {
     // Sistema de nível e XP
     protected int level, exp, nxtLevelXp;
 
-    // Inventário implementado com uma lista
+    // Inventário implementado com uma lista, junto de um variável para controlar o espaço
     protected List <Item> inventory = new ArrayList<>();
+    protected int maxInventorySize = 30;
 
     // Itens equipáveis: arma, armadura e capacete
     protected Equipment weapon;
@@ -87,30 +91,99 @@ public abstract class Character {
     }
 
     // Adicionando um item ao inventário (a lista)
-    public void addItem(Item item) {
-        inventory.add(item);
-        System.out.println("Você pegou: " + item.getItemName());
+    public boolean addItem(Item item) {
+        // Verifica se tem espaço antes de colocar
+        if (inventory.size() < maxInventorySize) {
+            inventory.add(item);
+            System.out.println("Você pegou: " + item.getItemName());
+            return true;
+        } else {
+            System.out.println("Inventário cheio!");
+            return false;
+        }
+    }
+
+    // Método para remover itens do inventário
+    public void dropItem(Item item) {
+        // Confere se o item está na mochila primeiro
+        if (inventory.contains(item)) {
+            inventory.remove(item);
+            System.out.println("Você jogou fora: " + item.getItemName());
+        } else {
+            System.out.println("Você não tem esse item na mochila!");
+        }
     }
 
     // Método para equipar itens equipáveis
     public void equipItem(Equipment newEquipment) {
-        // Caso o item seja uma arma
-        if (newEquipment.getType() == Equipment.Type.WEAPON) {
-            if (this.weapon != null) inventory.add(this.weapon);
-            this.weapon = newEquipment;
-        }
-        // Caso o item seja uma armadura
-        else if (newEquipment.getType() == Equipment.Type.ARMOR) {
-            if (this.armor != null) inventory.add(this.armor);
-            this.armor = newEquipment;
-        }
-        // Caso o item seja um capacete
-        else if (newEquipment.getType() == Equipment.Type.HELMET) {
-            if (this.helmet != null) inventory.add(this.helmet);
-            this.helmet = newEquipment;
-        }
-        // Depois que atribuímos o valor para weapon/armor/helmet, tiramos o item do inventário
+        // Se o item novo está na mochila, não ocupa espaço extra
         inventory.remove(newEquipment);
+        Equipment oldItem = null;
+
+        // Identifica se já tem algo equipado
+        if (newEquipment.getType() == Equipment.Type.WEAPON) oldItem = this.weapon;
+        else if (newEquipment.getType() == Equipment.Type.ARMOR) oldItem = this.armor;
+        else if (newEquipment.getType() == Equipment.Type.HELMET) oldItem = this.helmet;
+
+        // Se tiver, tentamos guardá-lo no inventário
+        if (oldItem != null) {
+            // Tenta adicionar no inventário
+            if (inventory.size() < maxInventorySize) {
+                inventory.add(oldItem);
+                System.out.println("Você guardou: " + oldItem.getItemName());
+            } else {
+                // Se não cabe o item antigo, cancela a operação
+                System.out.println("Iventário cheio! Não dá para trocar equipamento!");
+                inventory.add(newEquipment);
+                return; // Sai do método sem equipar
+            }
+        }
+
+        // Efetivando a ação de equipar
+        if (newEquipment.getType() == Equipment.Type.WEAPON) this.weapon = newEquipment;
+        else if (newEquipment.getType() == Equipment.Type.ARMOR) this.armor = newEquipment;
+        else if (newEquipment.getType() == Equipment.Type.HELMET) this.helmet = newEquipment;
+
+        System.out.println("Você equipou: " + newEquipment.getItemName());
+    }
+
+    public void unequipItem(Equipment.Type type) {
+        Equipment itemToRemove = null;
+
+        // Identificando o tipo de item a ser removido
+        if (type == Equipment.Type.WEAPON) itemToRemove = this.weapon;
+        else if (type == Equipment.Type.ARMOR) itemToRemove = this.armor;
+        else if (type == Equipment.Type.HELMET) itemToRemove = this.helmet;
+
+        // Verifica se tem algo para tirar
+        if (itemToRemove == null) {
+            System.out.println("Não há nada equipado nesse slot!");
+            return;
+        }
+
+        // Tenta guardar na mochila, se estiver cheia, não é retirado
+        if (this.addItem(itemToRemove)) {
+            if (type == Equipment.Type.WEAPON) this.weapon = null;
+            if (type == Equipment.Type.ARMOR) this.armor = null;
+            if (type == Equipment.Type.HELMET) this.helmet = null;
+
+            System.out.println("Você desequipou: " + itemToRemove.getItemName());
+        } else {
+            System.out.println("Esvazie o inventário antes de desequipar!");
+        }
+    }
+
+    // Método para usar itens no geral, chamando os métodos dos equipáveis e consumíveis
+    public void useItem(Item item) {
+        // Se for Equipamento, chama o método de equipar
+        if (item instanceof Equipment) {
+            this.equipItem((Equipment) item);
+        } 
+        // Se for consumível, aplica o efeito e remove da mochila
+        else  if (item instanceof Consumable) {
+            System.out.println(item.apply(this));
+            inventory.remove(item);
+        }
     }
 
     // Calcula o ataque total do jogador após o buff do item equipável
@@ -245,5 +318,33 @@ public abstract class Character {
         System.out.println("Agilidade: " + this.getEffectiveAgility());
         System.out.println("Inteligência: " + this.getEffectiveIntelligence());
         System.out.println("--------------------------------------------------------");
+    }
+
+    // Método para mostrar o inventário para o usuário
+    public void printInventory() {
+        // Confere se a mochila está vazia primeiro
+        if (inventory.isEmpty()) {
+            System.out.println("Mochila vazia!");
+            return;
+        }
+
+        System.out.println("===== MOCHILA (" + inventory.size() + "/" + maxInventorySize + ") =====");
+
+        // Cria um mapa onde a chave é o nome e o valor é a lista de itens com aquele nome
+        Map <String, List <Item>> groupedItems = inventory.stream().collect(Collectors.groupingBy(Item::getItemName));
+
+        // Iterar sobre os grupos para mostrar
+        int index = 1;
+        for (Map.Entry <String, List <Item>> entry : groupedItems.entrySet()) {
+            String nome = entry.getKey();
+            List <Item> listaDeItens = entry.getValue();
+
+            int quantidade = listaDeItens.size();
+            // Pegamos o preço do primeiro item da lista
+            int preco = listaDeItens.get(0).getItemPrice();
+
+            System.out.println(index + ". " + nome + " (x" + quantidade + ") - Valor: " + preco);
+            index++;
+        }
     }
 }
